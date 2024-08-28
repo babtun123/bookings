b5a59a2610af51cd851fe041b7df8602a9193de0
Samuel Shodiya <babtun123@gmail.com>
2024-08-27 23:54:23 -0500

    Added new images. Plus configured images to work using Go code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,14 +13,16 @@ func routes(app *config.AppConfig) http.Handler {
 
 	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	// _ = app
+	_ = app
 
 	mux := chi.NewRouter()
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	_ = app
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
